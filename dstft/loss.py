47d0aa74a8a8d8410535d787ba604d3a73ccd228
maxime <maxime.leiber0@gmail.com>
2023-06-20 08:28:10 +0200

    add speech

--- dstft/loss.py
+++ dstft/loss.py
@@ -11,6 +11,9 @@ def kurtosis_loss(x):
     kur = x.pow(4).mean(dim=1) / x.pow(2).mean(dim=1).pow(2) # B, T
     return kur #.mean()
 
+def kurtosis2_loss(x):
+    kur = x.pow(4).mean() / x.pow(2).mean().pow(2) 
+    return kur #.mean()
 
 
         
