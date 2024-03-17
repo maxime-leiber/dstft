7de7c04c65fe68fa9b853127c5044cf5aad532de
Maxime Leiber <maxime.leiber0@gmail.com>
2024-03-17 15:59:19 +0100

    aaa

--- dstft/__init__.py
+++
@@ -1,7 +0,0 @@
-from dstft.dstft import DSTFT, FDSTFT
-from dstft.loss import entropy_loss, kurtosis2_loss, kurtosis_loss
-from dstft.synchrosqueeze import fast_sst, sst2
-from dstft.tracking import frequency_tracking
-
-
-__version__ = '0.0.1'
--- setup.py
+++
@@ -1,7 +0,0 @@
-"""Package setup using `setuptools`.
-Please refer to the "setup.cfg" file for detailed properties of the package.
-"""
-import setuptools
-
-
-setuptools.setup()
